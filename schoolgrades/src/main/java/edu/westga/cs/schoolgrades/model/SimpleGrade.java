54aef32b37d0c721fa97d595cfd411326be24337
egrosch1 <egrosch1@csvlab-vm11.uwg.westga.edu>
2023-10-15 14:05:27 -0400

    Simple Grade Class

---
+++ schoolgrades/src/main/java/edu/westga/cs/schoolgrades/model/SimpleGrade.java
@@ -0,0 +1,24 @@
+package edu.westga.cs.schoolgrades.model;
+
+public class SimpleGrade implements Grade{
+
+	private double value;
+
+	/**
+	 * Getter for Value
+	 * @return the value
+	 */
+	public double getValue() {
+		return value;
+	}
+
+	/**
+	 * Setter for Value
+	 * @param value the value to set
+	 */
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	
+}
