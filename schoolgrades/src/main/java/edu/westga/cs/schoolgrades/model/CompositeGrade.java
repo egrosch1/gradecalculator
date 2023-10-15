beeed372fb4f4620eb4b4118ea3ea55421eef65b
egrosch1 <egrosch1@csvlab-vm11.uwg.westga.edu>
2023-10-15 14:21:48 -0400

    Added abstract class Composite grade and methods get value and add grade.

---
+++ schoolgrades/src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ */
+public abstract class CompositeGrade implements Grade, CalculationStrategy {
+	
+	private List<Grade> grades = new ArrayList<Grade>();
+	private double value;
+	
+	public void addGrade(Grade grade) {
+		this.grades.add(grade);
+	}
+	
+	/**
+	 * Getter for value
+	 * @return the value
+	 */
+	public double getValue() {
+		return value;
+	}
+}
