f1f4e44e4a746d453bba9dde3c493d7c96b58c1e
egrosch1 <egrosch1@csvlab-vm11.uwg.westga.edu>
2023-10-15 14:32:58 -0400

    Added SumOfGrades Subclass; had to create protected getter for grades

--- schoolgrades/src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
+++ schoolgrades/src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
@@ -12,6 +12,14 @@ import java.util.List;
 public abstract class CompositeGrade implements Grade, CalculationStrategy {
 	
 	private List<Grade> grades = new ArrayList<Grade>();
+	
+	/**
+	 * @return the grades
+	 */
+	protected List<Grade> getGrades() {
+		return grades;
+	}
+
 	private double value;
 	
 	public void addGrade(Grade grade) {
---
+++ schoolgrades/src/main/java/edu/westga/cs/schoolgrades/model/SumOfGrades.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * 
+ */
+public class SumOfGrades extends CompositeGrade{
+
+	@Override
+	public double getCalculatedGrade() {
+		double sum = 0;
+		for (Grade currentGrade : grades)
+			
+		return 0;
+	}
+
+	
+}
