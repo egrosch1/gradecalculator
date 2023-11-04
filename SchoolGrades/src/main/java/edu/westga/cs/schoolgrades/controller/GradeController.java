a623a2be44921342da9a3902df2121442d8e6865
egrosch1 <egrosch1@csvlab-vm02.uwg.westga.edu>
2023-11-04 11:04:18 -0400

    Updated gui fxml and contorller

--- SchoolGrades/src/main/java/edu/westga/cs/schoolgrades/controller/GradeController.java
+++ SchoolGrades/src/main/java/edu/westga/cs/schoolgrades/controller/GradeController.java
@@ -1,5 +1,43 @@
 package edu.westga.cs.schoolgrades.controller;
 
-public class GradeController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
+public class GradeController {
+    @FXML
+    private ListView<Integer> quizInput;
+    @FXML
+    private TextField quizSubtotalField;
+    @FXML
+    private VBox quizResultBox;
+    
+    @FXML
+    private ListView<Integer> homeworkInput;
+    @FXML
+    private TextField homeworkSubtotalField;
+    @FXML
+    private VBox homeworkResultBox;
+    
+    @FXML
+    private ListView<Integer> examInput;
+    @FXML
+    private TextField examSubtotalField;
+    @FXML
+    private VBox examResultBox;
+    
+    @FXML
+    private TextField finalGradeField;
+    @FXML
+    private Button recalculateButton;
+
+    @FXML
+    private void initialize() {
+    }
+
+    @FXML
+    private void recalculateButtonAction() {
+    }
 }
