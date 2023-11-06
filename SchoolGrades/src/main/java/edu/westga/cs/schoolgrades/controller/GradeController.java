c915e1419d4e9089c1d263e2c73fe8bbbcbe6968
egrosch1 <egrosch1@csvlab-vm09.uwg.westga.edu>
2023-11-06 15:59:08 -0500

    Got homework MOSTLY working (not dropping lowest yet.)

--- SchoolGrades/src/main/java/edu/westga/cs/schoolgrades/controller/GradeController.java
+++ SchoolGrades/src/main/java/edu/westga/cs/schoolgrades/controller/GradeController.java
@@ -9,7 +9,10 @@ import javafx.collections.ObservableList;
 
 import java.util.List;
 import java.util.Optional;
+
+import edu.westga.cs.schoolgrades.model.AverageOfGradesStrategy;
 import edu.westga.cs.schoolgrades.model.Grade;
+import edu.westga.cs.schoolgrades.model.GradeCalculationStrategy;
 import edu.westga.cs.schoolgrades.model.GradeListCell;
 import edu.westga.cs.schoolgrades.model.SimpleGrade;
 import edu.westga.cs.schoolgrades.model.SumOfGradesStrategy;
@@ -21,13 +24,29 @@ public class GradeController {
     private ObservableList<Grade> quizGrades;
 
     @FXML
+    private ListView<Grade> homeworkInput;
+    private ObservableList<Grade> homeworkGrades;
+    
+    @FXML
     private TextField quizSubtotalField;
+
+    @FXML
+    private TextField homeworkSubtotalField;
+    
+    private GradeCalculationStrategy homeworkCalculationStrategy;
+
 
     @FXML
     private void initialize() {
         this.quizGrades = FXCollections.observableArrayList();
         this.quizInput.setItems(this.quizGrades);
         this.quizInput.setCellFactory(param -> new GradeListCell());
+        
+        this.homeworkGrades = FXCollections.observableArrayList();
+        this.homeworkInput.setItems(this.homeworkGrades);
+        this.homeworkInput.setCellFactory(param -> new GradeListCell());
+        
+        this.homeworkCalculationStrategy = new AverageOfGradesStrategy();
     }
 
     @FXML
@@ -43,7 +62,7 @@ public class GradeController {
                 double gradeValue = Double.parseDouble(input);
                 Grade grade = new SimpleGrade(gradeValue);
                 this.quizGrades.add(grade);
-                updateSubtotal();
+                this.updateSubtotal();
             } catch (NumberFormatException e) {
                 
             }
@@ -52,12 +71,35 @@ public class GradeController {
 
     @FXML
     private void recalculateButtonAction() {
-        updateSubtotal();
+        this.updateSubtotal();
+    }
+    
+    @FXML
+    private void addHomeworkGrade() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Add Homework Grade");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Enter homework grade:");
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(input -> {
+            try {
+                double gradeValue = Double.parseDouble(input);
+                Grade grade = new SimpleGrade(gradeValue);
+                this.homeworkGrades.add(grade);
+                this.updateHomeworkSubtotal();
+            } catch (NumberFormatException e) {
+            }
+        });
+    }
+    @FXML
+    private void updateHomeworkSubtotal() {
+        double subtotal = this.homeworkCalculationStrategy.calculate(this.homeworkGrades);
+        this.homeworkSubtotalField.setText(String.valueOf(subtotal));
     }
 
     @FXML
     private void updateSubtotal() {
-        double subtotal = calculateTotal(this.quizGrades);
+        double subtotal = this.calculateTotal(this.quizGrades);
         this.quizSubtotalField.setText(String.valueOf(subtotal));
     }
 
