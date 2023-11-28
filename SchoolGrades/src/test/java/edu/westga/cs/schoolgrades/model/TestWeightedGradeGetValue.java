19aba205f242926cd296ba355a379255229a85ee
egrosch1 <egrosch1@csvlab-vm16.uwg.westga.edu>
2023-11-28 15:46:05 -0500

    Tutorial 2

--- SchoolGrades/src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeGetValue.java
+++ SchoolGrades/src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeGetValue.java
@@ -7,32 +7,32 @@ import org.mockito.Mockito;
 
 public class TestWeightedGradeGetValue {
 
-    private static final double DELTA = 0.001;
-    private WeightedGrade weightedGrade;
-    private Grade mockGrade;
+	private static final double DELTA = 0.001;
+	private WeightedGrade weightedGrade;
+	private Grade mockGrade;
 
-    @BeforeEach
-    public void setup() {
-        mockGrade = Mockito.mock(Grade.class);
-        
-        Mockito.when(mockGrade.getValue()).thenReturn(100.0);
-    }
+	@BeforeEach
+	public void setup() {
+		mockGrade = Mockito.mock(Grade.class);
 
-    @Test
-    public void shouldApplyZeroWeight() {
-        weightedGrade = new WeightedGrade(mockGrade, 0);
-        assertEquals(0, weightedGrade.getValue(), DELTA);
-    }
+		Mockito.when(mockGrade.getValue()).thenReturn(100.0);
+	}
 
-    @Test
-    public void shouldApplyWeightOfOne() {
-        weightedGrade = new WeightedGrade(mockGrade, 1);
-        assertEquals(100, weightedGrade.getValue(), DELTA);
-    }
+	@Test
+	public void shouldApplyZeroWeight() {
+		weightedGrade = new WeightedGrade(mockGrade, 0);
+		assertEquals(0, weightedGrade.getValue(), DELTA);
+	}
 
-    @Test
-    public void shouldApplyWeightBetweenZeroAndOne() {
-        weightedGrade = new WeightedGrade(mockGrade, 0.5);
-        assertEquals(50, weightedGrade.getValue(), DELTA);
-    }
-}
+	@Test
+	public void shouldApplyWeightOfOne() {
+		weightedGrade = new WeightedGrade(mockGrade, 1);
+		assertEquals(100, weightedGrade.getValue(), DELTA);
+	}
+
+	@Test
+	public void shouldApplyWeightBetweenZeroAndOne() {
+		weightedGrade = new WeightedGrade(mockGrade, 0.5);
+		assertEquals(50, weightedGrade.getValue(), DELTA);
+	}
+}	
