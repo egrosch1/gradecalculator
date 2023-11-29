f14edc9e0b3cb5c8e3425ecf30b830ad1003919b
egrosch1 <egrosch1@csvlab-vm12.uwg.westga.edu>
2023-11-29 12:15:52 -0500

    added mock objects to testsumofgradesstrategycalculate

--- SchoolGrades/src/test/java/edu/westga/cs/schoolgrades/model/TestSumOfGradesStrategyCalculate.java
+++ SchoolGrades/src/test/java/edu/westga/cs/schoolgrades/model/TestSumOfGradesStrategyCalculate.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,9 +24,9 @@ public class TestSumOfGradesStrategyCalculate {
 	
 	@BeforeEach
 	public void setup() {
-		grade0 = new SimpleGrade(10);
-		grade1 = new SimpleGrade(20);
-		grade2 = new SimpleGrade(30);
+		grade0 = mock(Grade.class);
+		grade1 = mock(Grade.class);
+		grade2 = mock(Grade.class);
 		
 		grades = new ArrayList<Grade>();
 		
